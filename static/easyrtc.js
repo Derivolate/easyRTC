aecd7c3aa930f249f0d313f9ca3ebd49cb6e32d8
XD-face-me <Bastiaan97.bvh@gmail.com>
2014-06-07 22:38:02 +0200

    cleaning up some stuff

--- static/easyrtc.js
+++ static/easyrtc.js
@@ -2,12 +2,15 @@ var selfEasyrtcid = "";
 var connected_rtcid;
 var connected_peers = [];
 function connect () {
-    easyrtc.setPeerListener (add_to_conversation); // Is called whenever there is sent data to this peer
-    easyrtc.setRoomOccupantListener (convert_list_to_buttons); // Is called every time a user joins the room
+    easyrtc.setPeerListener (add_to_conversation); // Is called whenever there
+                                                    // is sent data to this peer
+    easyrtc.setRoomOccupantListener (peer_connected); // Is called every time
+                                                        // a user joins the room
     easyrtc.easyApp ("easyrtc.audio_video", // The name of our application
     "self_video", // Id of the video element for our own media stream
 
-    [// Array containing the id's of the video elements for the incoming media stream
+    [// Array containing the id's of the video elements for the incoming
+        // media stream
             "cv1",
     ],
 
@@ -19,21 +22,22 @@ function connect () {
     function (errorCode, message) { // Initialisation failure callback
         easyrtc.showError (errorCode, message);
     });
-    
+
     button = document.getElementById ("send");
-    button.addEventListener('click',send_message);
+    button.addEventListener ('click', send_message);
 }
 
-function send_message(){
-    connected_peers.forEach(function(easy_rtcid){
-        console.log(easy_rtcid);
-        var text = document.getElementById("send_message_text").value;
-        send_stuff_WS(easy_rtcid, text);
+function send_message () {
+    connected_peers.forEach (function (easy_rtcid) {
+        console.log (easy_rtcid);
+        var text = document.getElementById ("send_message_text").value;
+        send_stuff_WS (easy_rtcid, text);
     });
 }
 
 function send_stuff_WS (easy_rtcid, text) {
-    if (text.replace (/\s/g, "").length === 0) { // Don"t send just whitespace
+    if (text.replace (/\s/g, "").length === 0) { // Don"t send just
+                                                    // whitespace
         return;
     }
     easyrtc.sendDataWS (easy_rtcid, "message", text);
@@ -48,27 +52,19 @@ function clear_connect_list () {
     }
 }
 
-function convert_list_to_buttons (roomName, data, isPrimary) {
+function peer_connected (roomName, data, isPrimary) {
     clear_connect_list ();
     connected_peers = [];
     var other_client_div = document.getElementById ("other_clients");
     for ( var easyrtcid in data) {
-        if( easyrtc.getConnectStatus(easyrtcid) === "not connected"){
-            perform_call(easyrtcid);
-        };
-        connected_peers.push(easyrtcid);
-//        var button = document.createElement ("button");
-//
-//        button.onclick = function (easyrtcid) {
-//            return function () {
-//                performCall (easyrtcid);
-//            };
-//        } (easyrtcid);
-//
+        if (easyrtc.getConnectStatus (easyrtcid) === "not connected") {
+            perform_call (easyrtcid);
+        }
+        ;
+        connected_peers.push (easyrtcid);
+
         var label = document.createTextNode (easyrtc.idToName (easyrtcid));
-        other_client_div.appendChild(label);
-//        button.appendChild (label);
-//        otherClientDiv.appendChild (button);
+        other_client_div.appendChild (label);
     }
 }
 
@@ -80,9 +76,29 @@ function perform_call (other_easyrtcid) {
     };
     easyrtc.call (other_easyrtcid, successCB, failureCB);
 }
-function add_to_conversation (who, msg_type, content) { // This function is used to add messages to the 'conversation' div
+function add_to_conversation (who, msg_type, content) { // This function is used
+                                                        // to add messages to
+                                                        // the 'conversation'
+                                                        // div
     // Replaces all html symbols with javascript symbols.
-    content = content.replace (/&/g, "&amp;")/* Ampercent */.replace (/</g, "&lt;")/* smaller then */.replace (/>/g, "&gt;") /* greater then */;
-    content = content.replace (/\n/g, "<br />") /* New line*/;
-    document.getElementById ("conversation").innerHTML += "<b>" + who + ":</b>&nbsp;" + content + "<br />"; // The actual chat mesage with the peer id in front of it
+    content = content.replace (/&/g, "&amp;")/* Ampercent */.replace (/</g, "&lt;")/*
+                                                                                     * smaller
+                                                                                     * then
+                                                                                     */.replace (/>/g, "&gt;") /*
+                                                                                                                                 * greater
+                                                                                                                                 * then
+                                                                                                                                 */;
+    content = content.replace (/\n/g, "<br />") /* New line */;
+    document.getElementById ("conversation").innerHTML += "<b>" + who + ":</b>&nbsp;" + content + "<br />"; // The
+                                                                                                            // actual
+                                                                                                            // chat
+                                                                                                            // mesage
+                                                                                                            // with
+                                                                                                            // the
+                                                                                                            // peer
+                                                                                                            // id
+                                                                                                            // in
+                                                                                                            // front
+                                                                                                            // of
+                                                                                                            // it
 }
